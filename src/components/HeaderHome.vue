548a3eacb4672f7d5e44b3ee03d852ad14ed5f8a
paulofranklins <paulofranklinc@gmail.com>
2025-07-12 01:45:35 -0700

    chore: remove duplicate and commented router-link from navbar

--- src/components/HeaderHome.vue
+++ src/components/HeaderHome.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <router-link class="navbar-brand" :to="{ name: 'home' }">
-        <!--        <img src="../assets/beth.png" height="40px" />-->
-      </router-link>
+<!--      <router-link class="navbar-brand" :to="{ name: 'home' }">-->
+<!--        &lt;!&ndash;        <img src="../assets/beth.png" height="40px" />&ndash;&gt;-->
+<!--      </router-link>-->
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
