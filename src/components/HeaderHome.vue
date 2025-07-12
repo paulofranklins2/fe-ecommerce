8dd23fb591027908276beafcafb9b4ce2e7073db
paulofranklins <paulofranklinc@gmail.com>
2025-07-11 23:01:11 -0700

    feat: add HeaderHome component with navbar and category links

---
+++ src/components/HeaderHome.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" :to="{ name: 'home' }">
+<!--        <img src="../assets/beth.png" height="40px" />-->
+      </router-link>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link :to="{ name: 'ListCategory' }">
+              <a class="nav-link" aria-current="page" href="#">Categories</a>
+            </router-link>
+          </li>
+        </ul>
+
+        <div>
+          <router-link :to="{ name: 'AddCategory' }">
+            <button class="btn btn-outline-success" type="submit">Add Category</button>
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "HeaderHome",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
