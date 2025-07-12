fcd77ac5c0f0eade8cd6aab84543c2bab9f004fa
paulofranklins <paulofranklinc@gmail.com>
2025-07-11 23:01:51 -0700

    refactor: replace HelloWorld with HeaderHome and dynamic routing with product/category data fetch

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav>
+    <HeaderHome></HeaderHome>
+    <router-view
+        v-if="products && categories"
+        :baseURL="baseURL"
+        :products="products"
+        :categories="categories"
+        @fetchData="fetchData"
+    ></router-view>
+  </nav>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import HeaderHome from "@/components/HeaderHome.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components: {HeaderHome},
+  data() {
+    return {
+      baseURL: 'http://localhost:8080/',
+      products: null,
+      categories: null,
+    };
+  },
+
+  methods: {
+    async fetchData() {
+      await axios.get(this.baseURL + 'product')
+          .then(response => this.products = response.data)
+          .catch(error => console.log(error));
+
+      await axios.get(this.baseURL + 'category')
+          .then(response => this.categories = response.data)
+          .catch(error => console.log(error));
+    }
+  },
+
+  mounted() {
+    this.fetchData();
   }
-}
+};
+
 </script>
 
 <style>
@@ -21,6 +51,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
