e44c3011e36d895d522458443d51d46ecee8202c
paulofranklins <paulofranklinc@gmail.com>
2025-07-15 10:42:39 -0700

    feat: add axios interceptor to include auth token from localStorage

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
-
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 
 window.axios = require('axios')
+
+window.axios.interceptors.request.use(config => {
+    let token = localStorage.getItem("token");
+    if (token) {
+        config.headers['Authorization'] = 'Bearer ' + token
+    }
+    return config
+});
+
 createApp(App).use(router).mount('#app')
