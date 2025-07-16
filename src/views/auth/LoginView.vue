873e9da94be37d1dc0c71c1bb4bf468103cf5422
paulofranklins <paulofranklinc@gmail.com>
2025-07-16 15:22:10 -0700

    feat(auth): integrate Google OAuth login using vue3-google-login
    - Added vue3-google-login dependency to package.json and package-lock.json
    - Updated LoginView.vue to implement Google OAuth login flow
    - Replaced manual email/password login with Google OAuth login method
    - Added button to trigger Google login in LoginView.vue template

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -16,12 +16,12 @@
           <form @submit.prevent="signin">
             <div class="form-group">
               <label>Email</label>
-              <input type="email" class="form-control" v-model="email" required/>
+              <input type="email" class="form-control" v-model="email" required />
             </div>
 
             <div class="form-group">
               <label>Password</label>
-              <input type="password" class="form-control" v-model="password" required/>
+              <input type="password" class="form-control" v-model="password" required />
             </div>
 
             <small class="form-text text-muted">
@@ -35,8 +35,10 @@
               </div>
             </button>
           </form>
+          <br />
+          <button @click="login" class="btn">Login Using Google</button>
 
-          <hr/>
+          <hr />
 
           <p class="text-center">
             <router-link :to="{ name: 'RegisterView' }" class="btn btn-dark w-100">
@@ -51,6 +53,7 @@
 
 <script>
 import axios from "axios";
+import { googleSdkLoaded } from "vue3-google-login";
 
 export default {
   data() {
@@ -72,7 +75,7 @@ export default {
 
       try {
         const res = await axios.post(this.baseURL + "auth/login", user, {
-          headers: {"Content-Type": "application/json"}
+          headers: { "Content-Type": "application/json" }
         });
         localStorage.setItem("token", res.data.token);
         this.$router.replace("/");
@@ -83,7 +86,41 @@ export default {
       } finally {
         this.loading = false;
       }
+    },
+
+    login() {
+      googleSdkLoaded(google => {
+        google.accounts.oauth2
+            .initCodeClient({
+              client_id: "701474785746-3r0e67th2cd6s2nb7n729o1gitgta3cr.apps.googleusercontent.com",
+              scope: "email profile openid",
+              redirect_uri: "http://localhost:3000",
+              callback: response => {
+                axios({
+                  method: "post",
+                  url: this.baseURL + "auth/oauth2/callback/google",
+                  data: JSON.stringify({ code: response.code }),
+                  headers: {
+                    "Content-Type": "application/json"
+                  }
+                })
+                    .then(res => {
+                      localStorage.setItem("token", res.data.token);
+                      this.$router.replace("/");
+                      console.log("Login successful. Please continue");
+                    })
+                    .catch(err => {
+                      console.error("Backend error:", err);
+                    });
+              }
+            })
+            .requestCode();
+      });
     }
+  },
+  mounted() {
+    this.loading = false;
+    window.handleCredentialResponse = this.handleCredentialResponse;
   }
 };
 </script>
